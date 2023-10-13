6bfd62543cbc84e64fc8f46b1e4e762b128909f9
Chasqui10 <chasqui.rmf@gmail.com>
2023-10-12 18:14:51 -0700

    HTML Script  pulling in the API calls

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,34 @@
 // HTML reference for sections
+const targetDaily = $('#dailyForecast');
+const targetWeekly = $('#weeklyForecast');
+const formContainer = $('#formContainer')
+const btnSearch = $('#btnSearch');
 
 
 
+// function to get API data from openweather.com  
+function getWeatherData(){
+    //var cityName = "Torrance"
+    //const apiKey = "e3d4af60a83c0902cca3fbece818aa9e"
+    //const weatherAPIUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`
+    
+    
+    // This Url use the City Name to pull the latittude and  longitude 
+    const weatherAPIUrl = `http://api.openweathermap.org/geo/1.0/direct?q=Phoenix&limit=1&appid=e3d4af60a83c0902cca3fbece818aa9e`
 
-var apiKey = "e3d4af60a83c0902cca3fbece818aa9e"
+    fetch(weatherAPIUrl)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data);
+    }) 
+};
 
+// function is needed on the weather search 
+
+
+// function for pulling text from the input form and  
+function WeatherSearch(){
+
+};
