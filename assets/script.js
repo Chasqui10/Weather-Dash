e84adfdb2df03a359946e1897a2f38936833edae
Chasqui10 <chasqui.rmf@gmail.com>
2023-10-16 14:56:32 -0700

    Update HTML,CSS troubleshooting containers and format

--- assets/script.js
+++ assets/script.js
@@ -3,25 +3,26 @@ const targetDaily = $('#dailyForecast');
 const targetWeekly = $('#weeklyForecast');
 const formContainer = $('#formContainer')
 const btnSearch = $('#btnSearch');
-
+const cityTarget = $('#citySearch');
 
 
 // function to get API data from openweather.com  
-function getWeatherData(){
+function getCityCoord(){
     //var cityName = "Torrance"
     //const apiKey = "e3d4af60a83c0902cca3fbece818aa9e"
     //const weatherAPIUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`
-    
+    cityName = cityTarget.val().trim();
     
     // This Url use the City Name to pull the latittude and  longitude 
-    const weatherAPIUrl = `http://api.openweathermap.org/geo/1.0/direct?q=Phoenix&limit=1&appid=e3d4af60a83c0902cca3fbece818aa9e`
+    const weatherLocApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=Phoenix&limit=1&appid=e3d4af60a83c0902cca3fbece818aa9e`
 
-    fetch(weatherAPIUrl)
+    fetch(weatherLocApiUrl)
     .then(function (response){
         return response.json();
     })
     .then(function (data){
         console.log(data);
+        getCityWeather(data);
     }) 
 };
 
@@ -29,6 +30,14 @@ function getWeatherData(){
 
 
 // function for pulling text from the input form and  
-function WeatherSearch(){
-
+function getCityWeather(data){
+    // cityLong = 
+    // cityLat = 
 };
+
+                
+
+
+                 
+ 
+               
